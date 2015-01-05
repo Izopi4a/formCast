80314e317031f615dbd4542f6f7b551f3cf0f2fe
Izopi4a <r1d3r@abv.bg>
2015-01-05 11:37:53 +0200

    reworked

--- cast.php
+++ cast.php
@@ -2,10 +2,13 @@
 
 namespace Forms {
 
-    class Cast {
+    class Layer {
 
         private $source = array();
         private $variableList = array();
+        private $min = false;
+        private $max = false;
+        private $default = false;
 
         public function __construct(array $source , array $variableList) {
             $this->variableList = $variableList;
@@ -23,24 +26,39 @@ namespace Forms {
 
             $outputData = array();
 
-            foreach ($variableList as $variable=>$type) {
-                if (is_array($type) && count($type) === 0) {
-                    $type = 'array';
-                } else if (is_array($type) && count($type) === 2){
+            foreach ($variableList as $variable => $type) {
+
+                $this->min = false;
+                $this->max = false;
+                $this->default = false;
+
+                if (is_array($type)){
+
+                    if (isset($type['name'])){
+
+                        $source[$type['name']] = $source[$variable];
+                        unset($source[$variable]);
+                        $variable = $type['name'];
+                    }
+
+                    if (isset($type['limit'])){
+                        $this->min = $type['limit'][0];
+                        $this->max = $type['limit'][1];
+                    }
+
+                    if (array_key_exists('default', $type)){
+                        $this->default = $type['default'];
+                    }
+
+                    $method = '_cast' . ucfirst($type['type']);
 
                     if (!isset($source[$variable])){
-                        $outputData[$type[0]] = $this->getDefaultValue('_cast' . ucfirst($type[1]));
+                        $outputData[$variable] = $this->getDefaultValue($method);
                         continue;
-                    } else {
-                        $source[$type[0]] = $source[$variable];
-                        unset($source[$variable]);
-
-                        $variable = $type[0];
-                        $type = $type[1];
                     }
+                } else {
+                    $method = '_cast' . ucfirst($type);
                 }
-
-                $method = '_cast' . ucfirst($type);
 
                 //check if is deffined 1st
                 if (isset($source[$variable]) === false){
@@ -49,6 +67,8 @@ namespace Forms {
 
                     if (method_exists($this, $method)){
                         $outputData[$variable] = $this->$method($source[$variable]);
+                    } else {
+                        throw new \Exception('unsupported form cast :'. $method);
                     }
                 }
             }
@@ -58,6 +78,10 @@ namespace Forms {
 
         private function getDefaultValue($method){
 
+            if ($this->default !== false){
+                return $this->default;
+            }
+
             if ($method === '_castArray'){
                 return array();
             } else if ($method === '_castInt'){
@@ -65,25 +89,40 @@ namespace Forms {
             } else if ($method === '_castString'){
                 return '';
             } else if ($method === '_castDate'){
-                return '';
+                return 0;
+            } else if ($method === '_castYear'){
+                return 0;
             } else if ($method === '_castBool'){
                 return 'false';
             } else if ($method === '_castFloat'){
                 return 0.00;
             } else if ($method === '_castDateToMysql'){
                 return '';
+            } else if ($method === '_castEmail'){
+                return '';
             } else if ($method === '_castForeignInt'){
                 return null;
-            } else if ($method === '_castDatePicker'){
-                return null;
-            } else if ($method === '_castText'){
-                return '';
+            } else if ($method === '_castFormatedInt'){
+                return 0;
+            } else if ($method === '_castTimeAddFullRepair'){
+                return NULL;
+            }else if ($method === '_castArrayInt'){
+                return array();
             }
 
             return '';
         }
 
         private function _castString($value) {
+
+            if ($this->min !== false && $this->min > strlen($value)){
+                return '';
+            }
+
+            if ($this->max !== false && $this->max < strlen($value)){
+                return '';
+            }
+
             $value = (string) $value;
 
             $replace = array(
@@ -92,18 +131,55 @@ namespace Forms {
                 '\\'
             );
 
-            return trim(stripcslashes(strip_tags(str_replace($replace,'',$value))));
+            $value = trim(stripcslashes(strip_tags(str_replace($replace,'',$value))));
+
+            if ($this->default !== false && $value === ''){
+                return $this->default;
+            }
+
+            return $value;
         }
 
         private function _castInt($value) {
-            return (int) $value;
+
+            $value = intval($value);
+
+            if ($this->min !== false && $this->min > $value){
+                return ($this->default !== false ? $this->default : 0 );
+            }
+            if ($this->max !== false && $this->max < $value){
+                return ($this->default !== false ? $this->default : 0 );
+            }
+
+            return $value;
+        }
+
+        private function _castFormatedInt($value) {
+
+            $value = str_replace("' ", '', $value);
+
+            return $this->_castInt($value);
         }
 
         private function _castFloat($value) {
-            return (float) str_replace(',','.', $value);
+
+            $value = floatval(str_replace(',','.', $value));
+
+            if ($value === 0.00 && $this->default !== false){
+                return $this->default;
+            }
+
+            if ($this->min !== false && $this->min > $value){
+                return ($this->default !== false ? $this->default : 0 );
+            }
+            if ($this->max !== false && $this->max < $value){
+                return ($this->default !== false ? $this->default : 0 );
+            }
+
+            return $value;
         }
 
-        private function _castArray($value) {
+        private function _castArrayInt($value) {
 
             if (is_array($value) && count($value) > 0){
                 foreach ($value AS $k => $v){
@@ -115,13 +191,14 @@ namespace Forms {
 
         }
 
-        private function _castForeignInt($value) {
+        private function _castArrayString($value) {
 
-            $value = $this->_castInt($value);
-
-            if ($value == 0){
-                $value = NULL;
+            if (is_array($value) && count($value) > 0){
+                foreach ($value AS $k => $v){
+                    $value[$k] = $this->_castString($v);
+                }
             }
+
             return $value;
 
         }
@@ -131,18 +208,13 @@ namespace Forms {
             $value = $this->_castString($value);
 
             $arr = explode('.', $value);
+            if (count($arr) !== 3) { return 0; }
 
-            if (count($arr) !== 3) { return ''; }
+            $dt = new \DateTime($value);
 
-            $day = $arr[0];
-            $month = $arr[1];
-            $year = $arr[2];
-
-            if ($day < 1 || $day > 31){ return ''; }
-            if ($month < 1 || $month > 12){ return '';}
-
-            return $value;
+            return $dt->getTimestamp();
         }
+
         private function _castDateToMysql($value){
 
             $value = $this->_castDate($this->_castString($value));
@@ -156,7 +228,7 @@ namespace Forms {
 
         private function _castBool($value){
 
-            return (boolean) $value;
+            return boolval($value);
         }
 
         private function _castYear($value){
@@ -168,7 +240,7 @@ namespace Forms {
                 return 0;
             }
 
-            if ($value < 1900){
+            if ($value < 1950 || $value > (date('Y') + 10)){
                 return 0;
             }
 
@@ -192,18 +264,29 @@ namespace Forms {
 
         }
 
-        private function _castDatePicker($value){
+        private function _castEmail($value){
 
-            $dt = new \DateTime($value);
+            $value = $this->_castString($value);
 
-            return $dt->format('Y-m-d H:i:s');
-
+            if(!filter_var($value, FILTER_VALIDATE_EMAIL)){
+                return '';
+            } else {
+                return $value;
+            }
         }
 
-        private function _castText($value){
+        private function _castForeignInt($value){
 
+            $value = $this->_castInt($value);
+
+            if ($value === 0){
+                return NULL;
+            }
             return $value;
+        }
 
+        private function _castPassword($value){
+            return $value;
         }
 
     }
